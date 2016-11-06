7f05d5af12cac3522b3a760c2dcff3c3fa8ad515
unagibushi <nuppeppo.0204@gmail.com>
2016-11-06 18:17:30 +0900

    23の日をお知らせする処理を実装

--- index.js
+++ index.js
@@ -6,14 +6,42 @@ if (!process.env.token) {
 }
 
 var Botkit = require('botkit');
-
+var CronJob = require('cron').CronJob;
 var controller = Botkit.slackbot({
     debug: false,
 });
 
 var bot = controller.spawn({
     token: process.env.token
-}).startRTM();
+}).startRTM(function(err, bot, payload) {
+
+    // 23の日をお知らせする処理
+    new CronJob({
+        cronTime: '0 7 * * *',
+        onTick: function() {
+            var text = '';
+            var today = new Date();
+            var day = today.getDate();
+            var leftDays = 23 - day;
+            if (leftDays === 0) {
+                text = '毎月23日は23の日です。'
+            } else if (leftDays <= 6 ) {
+                text = '23の日まで後' + leftDays + '日です。'
+            } else if ((day - 23) % 7 === 0) {
+                text = '23の日まで後' + (leftDays / 7) + '週間です。'
+            }
+
+            bot.say({
+                channel: 'general',
+                text: text,
+                username: 'wakaru',
+                icon_url: ''
+            });
+        },
+        start: true,
+        timeZone: 'Asia/Tokyo'
+    });
+});
 
 // ダイス
 controller.hears('\\d+[Dd]\\d*',
@@ -33,6 +61,6 @@ controller.hears('\\d+[Dd]\\d*',
             pipsList.push(pip);
         }
 
-        bot.reply(message,String(sum) + ' [' + pipsList.toString() + ']');
+        bot.reply(message, String(sum) + ' [' + pipsList.toString() + ']');
 
     });
